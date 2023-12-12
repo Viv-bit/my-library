aa1435b8c9760f260170af23b766515a2fecb321
Viv-bit <vivian.morenikenji@gmail.com>
2023-12-12 02:05:28 +0100

    finished up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import HomePage from "./pages/HomePage";
+import * as BooksAPI from "./BooksAPI";
+import Search from "./components/Search";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [mapOfIdToBooks, setMapOfIdToBooks] = useState(new Map());
+
+  const createMapOfBooks = (books) => {
+    const map = new Map();
+    books.map((book) => map.set(book.id, book));
+    return map;
+  };
+  useEffect(() => {
+    BooksAPI.getAll().then((res) => {
+      setBooks(res);
+      setMapOfIdToBooks(createMapOfBooks(res));
+    });
+  }, []);
+
+  const onBookShelfChange = (book, whereTo) => {
+    let id = book?.id;
+
+    const updatedBooks = books.map((book) => {
+      if (id === book.id) {
+        book.shelf = whereTo;
+        return book;
+      }
+      return book;
+    });
+    if (!mapOfIdToBooks.has(book.id)) {
+      book.shelf = whereTo;
+      updatedBooks.push(book);
+    }
+    setBooks(updatedBooks);
+
+    BooksAPI.update(book, whereTo);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Routes>
+        <Route
+          element={
+            <Search
+              books={books}
+              onBookShelfChange={onBookShelfChange}
+              createMapOfBooks={createMapOfBooks}
+            />
+          }
+          path="/search"
+        />
+
+        <Route
+          element={
+            <HomePage books={books} onBookShelfChange={onBookShelfChange} />
+          }
+          path="/"
+        />
+      </Routes>
     </div>
   );
 }
